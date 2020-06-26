7887322136fff5d0ebf5385b13d8aa88487b01b7
ethanjeong12 <jeonghb0212@gmail.com>
2020-06-26 20:21:58 +0900

    SignUp View: 크롤링 파일 삭제

--- order/views.py
+++ order/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import django.views import View
+import django.http  import JsonResponse
+
+from order.models   import *
+
+class OrderView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        
+        Order(
+             payment_info = Payment.name,
+        ).save()
+
+    def get(self, request):
+        payment_info = Payment.objects.values()
+        return JsonResponse({'Payment Method': payment_info}, status=200)
