eeea58e4f69f962aebd4552411566406f8e35f47
윤빛 <yoonbit@yunbichs-MacBook-Pro.local>
2020-06-27 13:25:03 +0900

    url 수정

--- lush/settings.py
+++ lush/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = lush_settings.SECRET["secret"]
+ALGORITHMS = 'HS256'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
