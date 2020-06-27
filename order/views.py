abec476f97e3e8c6eede2598dd06c76a1ff25d05
ethanjeong12 <jeonghb0212@gmail.com>
2020-06-27 12:59:04 +0900

    Order View: Payment view started

--- order/views.py
+++ order/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import django.views import View
+import django.http  import JsonResponse
+
+from order.models   import *
+
+class OrderView(View):
+    def post(self, request):
+        Order(
+             payment = Payment.name,
+        ).save()
+
+    def get(self, request):
+        payment_info = Payment.objects.values()
+        Order_list = {}
+
+        Order_list{
+
+
+        #return JsonResponse({'Payment Method': payment_info}, status=200)
