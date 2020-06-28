dbbed4746b8249154358ad0f358153baf790a763
윤빛 <yoonbit@yunbichs-MacBook-Pro.local>
2020-06-28 14:11:07 +0900

    views.py 따옴표 통일,utils.py token 변수 수정

--- lush/settings.py
+++ lush/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = lush_settings.SECRET["secret"]
-ALGORITHMS = 'HS256'
+ALGORITHMS = lush_settings.ALGORITHMS
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
