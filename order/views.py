02fb5c6698404a511aeeda052b3baf2c17432a54
윤빛 <yoonbit@yunbichs-MacBook-Pro.local>
2020-06-30 19:46:19 +0900

    배송지추가,수정,삭제 구현

--- order/views.py
+++ order/views.py
@@ -3,10 +3,10 @@ import json
 from django.http    import JsonResponse
 from django.views   import View
 
-from order.models   import Shipping
+from .models        import *
 
 class ShippingManagementView(View):
-   # @login_decorator
+    @login_decorator
     def post(self, request):
         data = json.loads(request.body)
         try:
@@ -17,28 +17,32 @@ class ShippingManagementView(View):
                     address         = data['address'],
                     phone_number    = data['phone_number']
                     ).save()
-            return JsonResponse({'message':'SUCCESS'}, status=200)
+            return JsonResponse({'message':'SUCCESS POST'}, status=200)
 
-        except KeyError:
-            return JsonResponse({'message': 'Invalid key.'})
+        except KeyError as e:
+            return JsonResponse({'message': 'Invalid key.'.e}, status = 401)
 
     def get(self, request):
         shipping_list = Shipping.objects.values()
         return JsonResponse({'comments':list(shipping_list)}, status=200)
 
     def delete(self, request):
-        data = json.loads(request.body)
-        shipping_delete = Shipping.objects.get(id = data['user_id'])
+        data            = json.loads(request.body)
+        shipping_delete = Shipping.objects.get(id = data['id'])
         shipping_delete.delete()
         return JsonResponse({'message':'DELETE SUCCESS'}, status=200)
     
-    def update(self,request):
-        data = json.loads(request.body)
-        shipping_update = Shipping.objects.update???(id = data['user_id'])
+    def put(self,request):
+        data            = json.loads(request.body)
+        name            = data['name']
+        shipping_update = Shipping.objects.get(id = data['id'])
         try:
-            Shipping(
-                    user_id      = data['user_id'],
-                    name         = data['name'],
-                    recipient    = data['recipient'],
-                    address      = data['address'],
-                    phone_number = data['phone_number']
+            shipping_update.name         = data['name']
+            shipping_update.address      = data['address']
+            shipping_update.recipient    = data['recipient']
+            shipping_update.phone_number = data['phone_number']
+            shipping_update.save()  
+            return JsonResponse({'message':'SUCCESS UPDATE'}, status=200)
+
+        except KeyError as e:
+            return JsonResponse({'message': 'Invalid key.'.e}, status = 401)
