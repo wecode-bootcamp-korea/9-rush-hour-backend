68cdd2df4f989cb51c85a8f9cc3f8c06e8b5fcef
monsterkos <monsterkos10@gmail.com>
2020-07-01 16:02:09 +0900

    Review 엔드 포인트 구현 중

--- review/views.py
+++ review/views.py
@@ -10,8 +10,21 @@ from user.utils     import login_decorator
 
 class ProductCommentView(View):
     def get(self, request, product_id):
-        pass
-        
+        reviews = Review.objects.select_related(
+            "product",
+            "user"
+        ).filter(product__product_number=product_id)
+        review_list = [
+            {
+                "stars"     :review.stars,
+                "date"      :review.date,
+                "review"    :review.reivew,
+                "user"      :review.user.name,
+                "product"   :review.product.name
+            }
+            for review in reviews
+        ]
+        return JsonResponse({"review":review_list}, status=200)
 
     #@login_decorator
     def post(self, request, product_id):
