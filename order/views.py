295b7f9f22cf43da46617abd8b28993b6cdcbcf2
ethanjeong12 <jeonghb0212@gmail.com>
2020-07-01 15:20:54 +0900

    OrderView 수정

--- order/views.py
+++ order/views.py
@@ -8,6 +8,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 #from user.utils             import login_decorator
 
+from user.models            import UserInfo 
 from order.models           import * 
 from product.models         import * 
 
@@ -16,11 +17,13 @@ class OrderView(View):
     def get(self, request):
         
         # bringing the user_id
-#        access_token = request.headers.get('Authorization')
-#        decoded_token = jwt.decode(access_token, 'secret', algorithm='HS256')
-#        decoded_user_id = decoded_token['id']
-        
-        user = UserInfo.objects.get(id=request.body.get('user_id'))
+        access_token = request.headers.get('Authorization')
+        decoded_token = jwt.decode(access_token, 'secret', algorithm='HS256')
+        decoded_user_id = decoded_token['id']
+
+        data = json.loads(request.body) 
+        #user = UserInfo.objects.get(id=data['user_id'])
+        user = UserInfo.objects.get(id=decoded_user_id)
 
         # displaying user info
         user_info = {
@@ -31,9 +34,10 @@ class OrderView(View):
             }
         
         # shipping info - default shipping address
-        shipping_method = request.body.get('shipping_method')
+        shipping_method = data['shipping_method']
         if shipping_method == 'default':
-            default_shipping = Shipping.objects.filter(basic=True).get(user_id=request.body.get('user_id'))
+            #default_shipping = Shipping.objects.filter(default=True).get(user_id=user)
+            default_shipping = Shipping.objects.filter(default=True).get(user_id=decoded_user_id)
 
             shipping_info = {
                 "recipient"    : default_shipping.recipient,
@@ -49,10 +53,16 @@ class OrderView(View):
                 "phone_number" : user.phone_number,
                 }
 
+        payment = Order.objects.filter(user_info_id = decoded_user_id).values()[0]
+        #payment = Order.objects.filter(user_info_id = data['user_id']).values()[0]
+        payment_method = {
+            "payment_method" : payment['payment_id']
+            }
+
         return JsonResponse({
-            "user_info"       : list(user_info),
-            "shipping_info"   : list(shipping_info),
-            "payment_method"  : list(Payment.objects.values('name')), # payment method
+            "user_info"       : user_info,
+            "shipping_info"   : shipping_info,
+            "payment_method"  : payment_method 
             }, status=200)
 
     #@login_decorator
@@ -63,17 +73,8 @@ class OrderView(View):
             # bringing the user_id
 #            access_token = request.headers.get('Authorization')
 #            decoded_token = jwt.decode(access_token, 'secret', algorithm='HS256')
-#            user_id = decoded_token['id']
+#            decoded_user_id = decoded_token['id']
             
-            # product info
-            random_order_no = "lush_"+uuid.uuid4().hex+"_"+datetime.strftime(datetime.today(),"%Y%m%d") 
-
-            order = Order.objects.create(user_info_id=data['user_id'], order_no=random_order_no, price=data['total_price'], payment_id=data['payment_id'], order_status_id=1, shipping_id=1)
-            
-            product_id_list = request.body.get('product_number')
-            for i in product_id_list:
-                OrderItem.objects.create(order=order, product=Product.objects.get(product_number=i))
-
             # shipping info - 직접입력
             ShippingInfo(
                 name = data["name"],
@@ -81,6 +82,34 @@ class OrderView(View):
                 phone_no = data["phone_no"],
                 message = data["message"]
             ).save() 
+
+            # product info
+            random_order_no = "lush_"+uuid.uuid4().hex+"_"+datetime.strftime(datetime.today(),"%Y%m%d") 
+            
+            order = Order.objects.create(
+                user_info_id = data['user_id'], 
+                order_no=random_order_no, 
+                price=data['total_price'], 
+                payment_id=data['payment_id'], 
+                order_status_id=data['order_status_id'], 
+                shipping_id= data['shipping_id']
+            )
+            
+#            order = Order.objects.create(
+#                user_info_id    = decoded_user_id, 
+#                order_no        = random_order_no, 
+#                price           = data['total_price'], 
+#                payment_id      = data['payment_id'], 
+#                order_status_id = (OrderStatus.objects.get(id=data['order_status_id']).id), 
+#                shipping_id     = (Shipping.objects.get(shipping_info_id=data['shipping_id']).id)
+#            )
+
+            product_id_list = data['product_number']
+            for i in product_id_list:
+                OrderItem.objects.create(order=order, product=Product.objects.get(product_number=i))
+
+                        
+            return JsonResponse({"message": "SUCCESSFULLY_SAVED"}, status = 200)
 
         except Order.DoesNotExist:
             return JsonResponse({"message": "INVALID_ORDER"}, status = 400)
